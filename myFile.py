6cd8b5a4ff4476b7a0cb827854f2a312318c5856
doyouknowk <kimdooyeon.87@gmail.com>
2024-09-09 00:59:48 +0900

    Update myFile.py
    가장 많이 당첨된 번호 추출 수량 변경

--- myFile.py
+++ myFile.py
@@ -1,5 +1,5 @@
 # 로또
 LAST_WIN_CHECK_TERM = 5     # 최근 당첨번호 획인 회차
 MOST_WIN_CHECK_TERM = 24    # 가장 많이 당첨된 번호 확인 회차
-MOST_WIN_NUM_COUNT = 8      # 가장 많이 당첨된 번호 추출 개수
+MOST_WIN_NUM_COUNT = 10      # 가장 많이 당첨된 번호 추출 개수
 RECOMMEND_SET_COUNT = 5     # 추천번호 세트 개수
